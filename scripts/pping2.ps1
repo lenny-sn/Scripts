a8f7b90ce8d09a561ce6cff5d783c95864230a22
lenny-sn <55696077+lenny-sn@users.noreply.github.com>
2019-09-24 19:53:05 +0700

    Delete pping2.ps1

--- scripts/pping2.ps1
+++
@@ -1,9 +0,0 @@
-param (
-[Parameter (Mandatory=$true, Position=1)]
-[string]$cname,
-
-[Parameter (Mandatory=$false, Position=2)]
-[int]$bsize=32
-)
-$a=1
-while ($a=1) {if (!(Test-Connection -ComputerName $cname -buffersize $bsize -Quiet)) {write-output ("Host "+$cname+" DOWN! ____ "+(get-date -format "HH:mm:ss  MM.dd.yyyy")+" ---by "+ $bsize+" byte")}}
